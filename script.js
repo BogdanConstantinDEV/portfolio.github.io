b8fb217bc31e0d0d47bd915c7e41c09af790ba9e
BogdanConstantin92 <bogdan_porojan@yahoo.com>
2020-01-21 22:03:34 +0200

    added animation to intro section and to navigation, and made the nav transparent

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLi = document.querySelectorAll('nav li');
 
+const introTitle = document.querySelector('.intro h1')
+const introSubtitle = document.querySelector('.intro p')
+const introImg = document.querySelector('.intro img')
+
 const services = document.querySelectorAll('.services > .service')
 
 
 
+
+// nav toggle
+
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 })
-
 navLi.forEach(item => {
     item.addEventListener('click', () => {
         document.body.classList.remove('nav-open')
@@ -17,6 +23,29 @@ navLi.forEach(item => {
 
 
 
+
+// intro animation
+
+const introOptions = {threshold: 1}
+const introObserver = new IntersectionObserver((entries, introObserver) => {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            return
+        } else {
+            entry.target.style.animation = `${entry.target.dataset.name} 1000ms linear forwards`
+        }
+        introObserver.unobserve(entry.target)
+    })
+}, introOptions)
+introObserver.observe(introTitle)
+introObserver.observe(introSubtitle)
+introObserver.observe(introImg)
+
+
+
+
+
+// what i use animation
 
 const serviceOptions = {rootMargin: '0px 0px -150px 0px', threshold: 1}
 const serviceObserver = new IntersectionObserver ((entries, serviceObserver) =>{
